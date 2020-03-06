0b8cb4c0fe56d65ffd85554a1ee92877eed9c5bb
Luis del Aguila <laguilaes@gmail.com>
2020-03-06 08:53:48 +0100

    Update basemodel.py

--- basemodel.py
+++ basemodel.py
@@ -12,7 +12,24 @@ import numpy as np
 
 
 os.chdir("C:/Users/ldelaguila/Documents/GitHub/Cholesk/m5-forecasting-accuracy")
+
+#%% Lectura de datos
 calendar=pd.read_csv('../m5-datos/calendar.csv', delimiter=",")
-sales=pd.read_csv('../m5-datos/sales_train_validation.csv', delimiter=",", nrows = 20)
+sales=pd.read_csv('../m5-datos/sales_train_validation.csv', delimiter=",")
 submission=pd.read_csv('../m5-datos/sample_submission.csv', delimiter=",")
-prices=pd.read_csv('../m5-datos/sell_prices.csv', delimiter=",", nrows=20)
+prices=pd.read_csv('../m5-datos/sell_prices.csv', delimiter=",")
+
+#%% Estadistica basica
+prices.wm_yr_wk.describe()
+
+
+#%% Creación TRAIN
+
+sales = sales.melt(id_vars=sales.iloc[:,list(range(0,6))].columns, var_name="d", value_name='Units').dropna(how='any').reset_index()
+
+train = pd.merge(sales, calendar, on='d')
+
+train = pd.merge(train, prices, on=['item_id', 'store_id', 'wm_yr_wk'])
+
+pd.set_option('display.max_columns', len(train))
+train.head()
